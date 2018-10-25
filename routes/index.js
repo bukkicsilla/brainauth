059488eb97a18cc8570794bdaf860e9ae05fea15
csilla <csilla.bukki@gmail.com>
2018-10-24 19:59:11 -0700

    login signup with routes

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var passport = require('passport'); 
 var router = express.Router();
 var brainController = require('../controllers/brain');
 
@@ -18,4 +19,28 @@ router.post('/updatemeaning/:brainpartid', brainController.updateMeaning);
 router.get('/updatefunctionalities/:brainpartid', brainController.formUpdateFunctionalities);
 router.post('/updatefunctionalities/:brainpartid', brainController.updateFunctionalities);
 
+router.get('/login', function(req, res, next) {  
+  res.render('login.ejs', { title: 'Login', message: req.flash('loginMessage') });
+});
+
+router.get('/signup', function(req, res) {  
+  res.render('signup.ejs', { title: 'Signup', message: req.flash('signupMessage') });
+});
+
+router.get('/profile', isLoggedIn, function(req, res) {  
+  res.render('profile.ejs', { title: 'Profile', user: req.user });
+});
+
+router.get('/logout', function(req, res) {  
+  req.logout();
+  res.redirect('/');
+});
+
 module.exports = router;
+
+function isLoggedIn(req, res, next) {  
+  if (req.isAuthenticated())
+      return next();
+  res.redirect('/');
+}
+//<title><%= title %></title>
